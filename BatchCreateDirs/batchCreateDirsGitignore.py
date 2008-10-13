be1dfdb0741047b5fc4d75f46fc44998ce5acc50
Dietmar Winkler <Dietmar.Winkler@TU-Berlin.de>
2008-10-13 19:34:45 +0200

    Major improvements in terms of fail safe and pyhtonic style thanks to FND!

--- BatchCreateDirs/batchCreateDirsGitignore.py
+++ BatchCreateDirs/batchCreateDirsGitignore.py
@@ -1,5 +1,18 @@
+#!/usr/bin/env python
+
 import os
-for line in open("list.txt").readlines():
-    directory = line.strip()
-    os.makedirs(directory)
-    f = open(directory+"/.gitignore","w");f.close()
+
+dirList = open("list.txt", "r")
+filename = ".gitignore"
+
+for dirPath in dirList:
+    # create empty directory
+    try:
+        os.makedirs(dirPath.strip())
+    except OSError: # directory already exists
+        pass
+    # create placeholder file if not present yet
+    f = open(dirPath.strip()+os.sep+filename, "a+")
+    f.close()
+
+dirList.close()
