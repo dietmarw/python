8508b8b0a8fdf6eff69a218146a17d8399125091
Dietmar Winkler <Dietmar.Winkler@TU-Berlin.de>
2008-10-18 13:26:47 +0200

    Documentation added.

--- BatchCreateDirs/batchCreateDirsGitignore.py
+++ BatchCreateDirs/batchCreateDirsGitignore.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
+"""
+This script parses the 'git svn' "unhandled.log" file for
+all empty directories that have been created and are ignored
+by git. It then creates _all_ of those directories and places
+a ".gitignore" file into them. That way one can make sure that
+his 'git svn' checkout will behave just like a svn checkout would
+(e.g., some badly written Makefile might rely on empty dirs during
+buil process).
 
+Usage: python batchCreateDirsGitignore.py /path/to/trunk/unhandled.log
+
+Shortcommings:
+	* This is hardcoded for use on "unhandled.log" files from the
+		tunk branch of svn.
+	* It will create _all_ empty directories that have ever been
+		during the subversion history (also those that have been
+		removed a a later time). Although this is does not brake
+		it will leave you with more empty dirs in your git checkout
+		than are actually present in the current svn repo.
+	* It is probably worth checking the current state of the svn repo
+		and then simply remove empty dirs which were removed during
+		svn history.
+"""
 import sys
 import os
 import re
