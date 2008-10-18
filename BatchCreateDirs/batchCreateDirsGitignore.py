bd299f1cf57379decd637341c8c36506c32f5692
Dietmar Winkler <Dietmar.Winkler@TU-Berlin.de>
2008-10-18 12:12:08 +0200

    Switched from spaces to tabs

--- BatchCreateDirs/batchCreateDirsGitignore.py
+++ BatchCreateDirs/batchCreateDirsGitignore.py
@@ -6,13 +6,13 @@ dirList = open("list.txt", "r")
 filename = ".gitignore"
 
 for dirPath in dirList:
-    # create empty directory
-    try:
-        os.makedirs(dirPath.strip())
-    except OSError: # directory already exists
-        pass
-    # create placeholder file if not present yet
-    f = open(dirPath.strip()+os.sep+filename, "a+")
-    f.close()
+	# create empty directory
+	try:
+		os.makedirs(dirPath.strip())
+	except OSError: # directory already exists
+		pass
+	# create placeholder file if not present yet
+	f = open(dirPath.strip()+os.sep+filename, "a+")
+	f.close()
 
 dirList.close()
