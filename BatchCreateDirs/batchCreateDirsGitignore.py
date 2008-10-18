0cbfe35cba21ef6b8aa00e1301f9ae8ca2d25c42
Dietmar Winkler <Dietmar.Winkler@TU-Berlin.de>
2008-10-18 13:17:18 +0200

    Script can now parse the original unhandled.log file directly

--- BatchCreateDirs/batchCreateDirsGitignore.py
+++ BatchCreateDirs/batchCreateDirsGitignore.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
 
+import sys
 import os
+import re
 
-dirList = open("list.txt", "r")
-filename = ".gitignore"
+def main(argv):
+	dirList = open(argv[1], "r")
+	filename = ".gitignore"
+	for dirPath in dirList:
+		# create empty directory
+		if "+empty_dir:" in dirPath:
+			try:
+				emptyPath=re.sub("^.*\+empty_dir:\strunk/","",dirPath)
+				print emptyPath
+				os.makedirs(emptyPath.strip())
+			except OSError: # directory already exists
+					pass
+				# create placeholder file if not present yet
+			f = open(emptyPath.strip()+os.sep+filename, "a+")
+			f.close()
+	dirList.close()
 
-for dirPath in dirList:
-	# create empty directory
-	try:
-		os.makedirs(dirPath.strip())
-	except OSError: # directory already exists
-		pass
-	# create placeholder file if not present yet
-	f = open(dirPath.strip()+os.sep+filename, "a+")
-	f.close()
-
-dirList.close()
+if __name__ == "__main__":
+	sys.exit(main(sys.argv))
